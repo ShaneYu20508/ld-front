72add221eb77756f3c175e5b8c0d61388bd512ec
ShaneYu20508 <145088890+ShaneYu20508@users.noreply.github.com>
2024-02-23 07:16:29 +0000

    Deploying to gh-pages from @ ShaneYu20508/ld-front@d21af1ce30e5050686927f3285d179f9a4700def 🚀

---
+++ assets/ssrBoot-424ff488.js
@@ -0,0 +1,1 @@
+import{s,A as t,o as e,aT as n}from"./index-5fdb30a4.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
